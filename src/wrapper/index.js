06c02cb449e0e2b08577c83c232d74984a9974d1
zy410419243 <zy410419243@gmail.com>
2018-09-25 20:55:31 +0800

    update

--- src/wrapper/index.js
+++ src/wrapper/index.js
@@ -2,7 +2,7 @@
  * @Author: zy9@github.com/zy410419243
  * @Date: 2018-04-24 15:34:46
  * @Last Modified by: zy9
- * @Last Modified time: 2018-08-29 17:31:23
+ * @Last Modified time: 2018-09-25 19:34:59
  */
 // import DataInstance from '../core/data-instance';
 
@@ -12,25 +12,6 @@ fetch('./mock/frameDatas.json')
 	.then(result => result.json())
 	.then(result => {
 		const { data } = result;
-
-		// document.getElementById('wrapper_1').addEventListener('dragstart', event => {
-		// 	event.dataTransfer.setData('Text', event.target.id);
-		// }, false);
-
-		// document.getElementById('wrapper_2').addEventListener('dragenter', event => {
-		// 	event.preventDefault();
-		// }, false);
-
-		// document.getElementById('wrapper_2').addEventListener('dragover', event => {
-		// 	event.preventDefault();
-		// }, false);
-
-		// document.getElementById('wrapper_2').addEventListener('drop', event => {
-		// 	event.preventDefault();
-		// 	const data = event.dataTransfer.getData('Text');
-
-		// 	event.target.innerText = 'test';
-		// }, false);
 
 		initWrapper(data);
 
@@ -44,12 +25,33 @@ fetch('./mock/frameDatas.json')
 
 		recurseIframeLoad(execObjects, 0, () => {
 			console.log('finished');
+
+			document.getElementById('wrapper_1').addEventListener('dragstart', event => {
+				event.dataTransfer.setData('Text', event.target.id);
+			}, false);
+
+			document.getElementById('wrapper_2').addEventListener('dragenter', event => {
+				event.preventDefault();
+			}, false);
+
+			document.getElementById('wrapper_2').addEventListener('dragover', event => {
+				event.preventDefault();
+			}, false);
+
+			document.getElementById('wrapper_2').addEventListener('drop', event => {
+				event.preventDefault();
+				const data = event.dataTransfer.getData('Text');
+
+				console.log(data);
+
+				event.target.innerText = 'test';
+			}, false);
 		});
 	});
 
 /**
  * 初始化根节点及模块容器
- * @param options 配置数据
+ * @param options 配置数据，[], 参数尚未确定
  */
 const initWrapper = options => {
 	const root = document.getElementById('root');
@@ -58,7 +60,7 @@ const initWrapper = options => {
 		const { left, top, width, height, style, id } = item;
 
 		root.innerHTML += `
-			<div style='left: ${ left }; top: ${ top }; width: ${ width }; height: ${ height }; ${ style }' id='wrapper_${ id }' draggable='true' class='wrapper'>
+			<div style='left: ${ left }; top: ${ top }; width: ${ width }; height: ${ height }; ${ style }' id='wrapper_${ id }' class='wrapper' draggable='true'>
 				<div class='title' style='cursor: default;'>
 					<span>title_${ id }</span>
 				</div>
@@ -67,6 +69,7 @@ const initWrapper = options => {
 		`;
 	}
 };
+
 /**
  * iframe按顺序加载
  * @param execObjects [{ node: DomNode, url: string, method: [{ key: string, params: {} }] }]
