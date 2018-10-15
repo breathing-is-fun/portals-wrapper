d278f705f7d793e82bc3693227137d2c4f8e07d1
zy410419243 <zy410419243@gmail.com>
2018-10-15 15:39:45 +0800

    display release

---
+++ dist/thirdModules/zbzx/index.js
@@ -0,0 +1,23 @@
+export class TestModule {
+	constructor (root) {
+		let wrapper = document.createElement('div');
+
+		wrapper.innerHTML = `
+            <img src='http://47.95.1.229:9003/UploadFile/201808/zbzx.png' style='width: 100%; height: 100%;' />
+        `;
+		wrapper.setAttribute('style', 'background: #F96; height: 100%; transition: background .7s cubic-bezier(.645, .045, .355, 1);');
+
+		this.wrapper = wrapper;
+		this['_moduleOnMount'] = this.onMount;
+
+		root.appendChild(wrapper);
+	}
+
+	onMount () {
+
+	}
+
+	changeBackgroundColor (color) {
+		this.wrapper.style.backgroundColor = color;
+	}
+}
