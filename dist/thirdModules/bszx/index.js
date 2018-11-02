5d4e89bd0a3b0c89268e4511dd0c5971bbfc3fba
zy410419243 <zy410419243@gmail.com>
2018-11-02 10:22:30 +0800

    release

---
+++ dist/thirdModules/bszx/index.js
@@ -0,0 +1,13 @@
+export class TestModule {
+	constructor (root) {
+		this['_moduleOnMount'] = this.onMount;
+
+		root.innerHTML = `
+			<img src='http://47.95.1.229:9003/UploadFile/201808/bszx.png' style='width: 100%; height: 100%;' />
+		`;
+	}
+
+	onMount () {
+
+	}
+}
