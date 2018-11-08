a469d92f6a00d91daa5fc362351b169709b640b4
zy410419243 <zy410419243@gmail.com>
2018-11-08 21:14:33 +0800

    Login添加临时过渡动画

--- src/modules/Login/index.js
+++ src/modules/Login/index.js
@@ -2,11 +2,20 @@
  * @Author: zy9@github.com/zy410419243
  * @Date: 2018-11-08 19:18:05
  * @Last Modified by: zy9
- * @Last Modified time: 2018-11-08 19:29:37
+ * @Last Modified time: 2018-11-08 21:13:14
  */
 import React, { Component } from 'react';
 
 import './css/Login.css';
+
+const datas = [
+	['你们体会过吉娃娃丢了的感觉吗？', '我家的是杂交的', '巨小', '找了一下午没找到', '回家爬在桌子上正要号啕大哭的时候', '妈的抽屉里突然传来犬吠...'],
+	['人家才子看到美景会说', '落霞与孤鹜齐飞，秋水共长天一色', '而你只会说', '卧槽真鸡儿好看'],
+	['谢邀', '人在美国，刚下飞机', '博士学位', '月入过亿', '从小学习舞蹈国画乐器', '家父是一名黑道大哥', '由于某种不幸的原因开始写起了bug', '---------分割线---------', '评论区戾气太重', '圈内人士太多', '利益相关', '匿了', '侵删', '以上'],
+	['机场验票', '"先生，您脸上这道疤是怎么回事？"', '"当年我妈剖腹产……"'],
+	['从前有座山', '山上有座庙', '庙里有个老和尚和小和尚', '有一天小和尚对老和尚说', '"爸爸，外面下雪了！"'],
+	['小老弟', '有些事情', '要手当其冲啊']
+];
 
 export default class index extends Component {
 	constructor (props) {
@@ -14,17 +23,32 @@ export default class index extends Component {
 
 		this.state = {
 			ticket: '参数获取中...',
+			showDatas: datas[~~(Math.random() * 100) % datas.length],
+			index: -1,
 		};
-
-		if(!window.SCTool) {
-			window.SCTool = {};
-		}
 	}
 
     componentDidMount = () => {
+    	let { showDatas, index } = this.state;
     	const ticket = this.getParams('ticket');
 
-    	this.setState({ ticket });
+    	if(!window.SCTool) {
+    		window.SCTool = {};
+    	}
+
+    	window.SCTool.ticket = ticket;
+
+    	showDatas.push(`这是你要的ticket，请查收：${ ticket }`);
+
+    	this.setState({ showDatas }, () => {
+    		setTimeout(() => {
+    			setInterval(() => {
+    				if(index < showDatas.length - 1) {
+    					this.setState({ index: ++index });
+    				}
+    			}, 2000);
+    		}, 500);
+    	});
     }
 
     getParams = name => {
@@ -39,11 +63,19 @@ export default class index extends Component {
     }
 
     render = () => {
-    	const { ticket } = this.state;
+    	const { showDatas, index } = this.state;
 
     	return (
     		<div className='Login' style={{ height: document.documentElement.clientHeight || document.body.clientHeight }}>
-    			{ ticket }
+    			<ul>
+    				{
+    					showDatas.map((item, i) => {
+    						return (
+    							<li key={ `content${ i }` } style={{ transform: `translateY(${ -(index * 70) }px)`, width: '100%', height: '100%', transition: 'all 0.5s cubic-bezier(0.645, 0.045, 0.355, 1)' }}>{ item }</li>
+    						);
+    					})
+    				}
+    			</ul>
     		</div>
     	);
     }
