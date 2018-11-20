be636f32fe01e1388bff06f2968b004feb087fa6
zy410419243 <zy410419243@gmail.com>
2018-11-20 16:38:49 +0800

    test: update test cases and snapshots

--- src/component/Ruler/__tests__/Ruler.test.js
+++ src/component/Ruler/__tests__/Ruler.test.js
@@ -2,7 +2,7 @@
  * @Author: zy9@github.com/zy410419243
  * @Date: 2018-10-24 17:14:50
  * @Last Modified by: zy9
- * @Last Modified time: 2018-10-24 19:17:29
+ * @Last Modified time: 2018-11-20 16:36:17
  */
 import React from 'react';
 import { render, mount, shallow } from 'enzyme';
@@ -11,6 +11,12 @@ import Ruler from '..';
 describe('Ruler', () => {
 	it('render correctly', () => {
 		const wrapper = mount(<Ruler>test</Ruler>);
+
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('padding should work', () => {
+		const wrapper = mount(<Ruler padding={ 20 }>test</Ruler>);
 
 		expect(wrapper).toMatchSnapshot();
 	});
