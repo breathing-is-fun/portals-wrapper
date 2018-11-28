74ee8efda9b24bba7db12eb5bf4414b4b8b36365
zy410419243 <zy410419243@gmail.com>
2018-11-28 17:26:29 +0800

    chore: remove useless code

--- src/index.js
+++ src/index.js
@@ -11,18 +11,6 @@ import GlobalListener from './localStorage/GlobalListener';
 
 const MOUNT_NODE = document.getElementById('root');
 
-// 不同路由间传递参数的对象，为防止不兼容，这里只保存非持久化数据
-// 后续改为绑定监听，这样太丑陋
-window['_acrossDatas'] = {
-	moduleToComponent: { // 模块编辑页跳转到组件编辑页，后者需要的参数
-		data: {},
-	},
-	componentToModule: {
-		isComponentSave: false, // 模块编辑页跳转到组件编辑页后，组件编辑保存后需要跳转模块编辑页，这是组件编辑是否完成的标识
-		data: {},
-	}
-};
-
 new Store(null, store => {
 	ReactDOM.render((
 		<div>
