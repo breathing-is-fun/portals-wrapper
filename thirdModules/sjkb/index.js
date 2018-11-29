bb98ddd0f4db5f00774f8e2f868a88d102b4e6d5
zy410419243 <zy410419243@gmail.com>
2018-11-29 19:48:15 +0800

    chore: update third-party components

--- thirdModules/sjkb/index.js
+++
@@ -1,5 +0,0 @@
-var Sjkb = function (root) {
-	root.innerHTML = '<img src=\'http://47.95.1.229:9003/UploadFile/201808/sjkb.png\' style=\'width: 100%; height: 100%;\' />';
-};
-
-module.exports = Sjkb;
