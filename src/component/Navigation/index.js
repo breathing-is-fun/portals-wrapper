9fa3a6296b8c49037c51777e8a22170be848143e
zy410419243 <zy410419243@gmail.com>
2018-12-05 14:24:27 +0800

    style: improve code style

--- src/component/Navigation/index.js
+++ src/component/Navigation/index.js
@@ -44,93 +44,90 @@ export default class Navigation extends Component {
 	  this.setState({ iconRotate, dropDownVisible });
 	}
 
-    render = () => {
-    	const { children, datas, onClick, clock, title } = this.props;
-    	const { CurrentTime, dropDownVisible, iconRotate } = this.state;
+	render = () => {
+	  const { children, datas, onClick, clock, title } = this.props;
+	  const { CurrentTime, dropDownVisible, iconRotate } = this.state;
 
-    	// 分割线
-    	// const space = <div className='content-space' />;
+	  const date = (
+	    <div className='content-wrapper'>
+	      <div className='content-time-detail'>
+	        { moment().format('YYYY-MM-DD') }
+	      </div>
+	      <div className='content-time-detail'>
+	        { moment().format('dddd') }
+	      </div>
+	    </div>
+	  );
 
-    	const date = (
-    		<div className='content-wrapper'>
-    			<div className='content-time-detail'>
-    				{ moment().format('YYYY-MM-DD') }
-    			</div>
-    			<div className='content-time-detail'>
-    				{ moment().format('dddd') }
-    			</div>
-    		</div>
-    	);
+	  const switchMeal = (
+	    <Menu>
+	      {
+	        datas.map(item => {
+	          const { title, id } = item;
 
-    	const switchMeal = (
-    		<Menu>
-    			{
-    				datas.map(item => {
-    					const { title, id } = item;
+	          return (
+	            <Menu.Item
+	              key={ id }
+	              onClick={ e => onClick(item) }
+	            >
+	              <a rel='noopener noreferrer'>{ title }</a>
+	            </Menu.Item>
+	          );
+	        })
+	      }
+	    </Menu>
+	  );
 
-    					return (
-    						<Menu.Item
-    							key={ id }
-    							onClick={ e => onClick(item) }
-    						>
-    							<a rel='noopener noreferrer'>{ title }</a>
-    						</Menu.Item>
-    					);
-    				})
-    			}
-    		</Menu>
-    	);
+	  return (
+	    <div className='Navigation'>
+	      <div className='content'>
+	        <div
+	          className='content-wrapper'
+	          style={{ width: 300 }}
+	        >
+	          { title }
+	        </div>
 
-    	return (
-    		<div className='Navigation'>
-    			<div className='content'>
-    				<div
-    					className='content-wrapper'
-    					style={{ width: 300 }}
-    				>
-    					{ title }
-    				</div>
+	        <div className='content-wrapper' style={{ width: 300 }}>
+	          <div className='content-time'>
+	            { CurrentTime && <CurrentTime /> }
+	          </div>
+	        </div>
 
-    				<div className='content-wrapper' style={{ width: 300 }}>
-    					<div className='content-time'>
-    						{ CurrentTime && <CurrentTime /> }
-    					</div>
-    				</div>
+	        { clock && date }
 
-    				{ clock && date }
-
-    				<div
-    					className='content-wrapper content-switch'
-    				>
-    					<Dropdown
-    						overlay={ switchMeal }
-    						trigger={ ['hover', 'click'] }
-    						onVisibleChange={ this.handleIconType }
-    						visible={ dropDownVisible }
-    					>
-    						<span className='droplink'>
+	        <div
+	          className='content-wrapper content-switch'
+	        >
+	          <Dropdown
+	            overlay={ switchMeal }
+	            trigger={ ['hover', 'click'] }
+	            onVisibleChange={ this.handleIconType }
+	            visible={ dropDownVisible }
+	          >
+	            <span className='droplink'>
 								套餐切换
-    							<Icon
-    								type='down'
-    								style={{
-    									transform: `rotate(${ iconRotate }deg)`,
-    								}}
-    								className='droplink-icon'
-    							/>
-    						</span>
-    					</Dropdown>
-    				</div>
-    			</div>
+	              <Icon
+	                type='down'
+	                style={{
+	                  transform: `rotate(${ iconRotate }deg)`,
+	                }}
+	                className='droplink-icon'
+	              />
+	            </span>
+	          </Dropdown>
+	        </div>
+	      </div>
 
-    			<div
-    				className='children-wrapper'
-    				style={{ height: (document.body.clientHeight - 65) }}
-    			>
-    				{ children }
-    			</div>
-    		</div>
-    	);
-    }
+	      <div
+	        className='children-wrapper'
+	        style={{ height: (document.body.clientHeight - 65) }}
+	      >
+	        { children }
+	      </div>
+	    </div>
+	  );
+	}
 }
 
 Navigation.propTypes = {
